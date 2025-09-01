72086ffa04ed9c6bc268333bf6328d11967b3676
Ashutosh Windows <ashutoshdhote6@gmail.com>
2025-09-01 16:14:29 +0530

    OS module

--- os_module.py
+++ os_module.py
@@ -3,11 +3,9 @@ import os
 #OS name/info
 os_info = os.uname()
 os_name = os.uname().sysname
-print(os_info)
-print(os_name)
+print(f'OS Info: {os_info}')
+print(f'OS Name: {os_name}')
 
 #Current Working Directory
 cwd = os.getcwd()
 print("Current working dir: ", cwd)
-
-
