33816381b3a88f838b71941fa2ee32c774c843ee
Ashutosh Windows <ashutoshdhote6@gmail.com>
2025-09-03 16:10:38 +0530

    OS module

--- os_module.py
+++ os_module.py
@@ -9,3 +9,17 @@ print(f'OS Name: {os_name}')
 #Current Working Directory
 cwd = os.getcwd()
 print("Current working dir: ", cwd)
+
+#List Files/Directories
+print(os.listdir('.'))
+print("list of files/folders in cwd:", os.listdir(cwd))
+
+# Check if File exist or not?
+try:
+    filename = 'abc.txt'
+    f = open(filename, 'r')
+    text = f.read()
+    f.close()
+except IOError as e:
+    print(f"Not accessed or problem in reading:" + filename +"\n", {e})
+
