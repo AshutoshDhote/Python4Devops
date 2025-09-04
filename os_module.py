024d8eac373d84f855108e6f61d06d94759b8205
Ashutosh Windows <ashutoshdhote6@gmail.com>
2025-09-04 15:10:17 +0530

    OS module

--- os_module.py
+++ os_module.py
@@ -1,25 +1,47 @@
 import os
 
-#OS name/info
-os_info = os.uname()
-os_name = os.uname().sysname
-print(f'OS Info: {os_info}')
-print(f'OS Name: {os_name}')
+def osname_listdir():
 
-#Current Working Directory
-cwd = os.getcwd()
-print("Current working dir: ", cwd)
+    #OS name/info
+    os_info = os.uname()
+    os_name = os.uname().sysname
+    print(f'OS Info: {os_info}')
+    print(f'OS Name: {os_name}')
 
-#List Files/Directories
-print(os.listdir('.'))
-print("list of files/folders in cwd:", os.listdir(cwd))
+    #Current Working Directory
+    cwd = os.getcwd()
+    print("Current working dir: ", cwd)
 
-# Check if File exist or not?
-try:
-    filename = 'abc.txt'
-    f = open(filename, 'r')
-    text = f.read()
-    f.close()
-except IOError as e:
-    print(f"Not accessed or problem in reading:" + filename +"\n", {e})
+    #List Files/Directories
+    print(os.listdir('.'))
+    print("list of files/folders in cwd:", os.listdir(cwd))
 
+    # Check if File exist or not?
+    try:
+        filename = 'abc.txt'
+        f = open(filename, 'r')
+        text = f.read()
+        f.close()
+    except IOError as e:
+        print(f"Not accessed or problem in reading:" + filename +"\n", {e})
+
+#osname_listdir()
+
+# Sort the files in current working directory
+
+def soft_files():
+    files_n_dirs = os.listdir('.')
+    print (files_n_dirs)
+
+    #VALID FILES CHECK
+    files = [item for item in files_n_dirs if os.path.isfile(os.path.join(os.getcwd(),item))]
+    print(f'Files: {files}')
+    sorted_files = sorted(files)
+
+    #Print one by one
+    if sorted_files:
+        for f in sorted_files:
+            print(f'- {f}')
+
+
+soft_files()
